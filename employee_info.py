83543fce8ba19d74e8561e64d14137f655d32e52
aqil <ahmadaqilyusop@gmail.com>
2024-05-06 22:36:01 +0800

    Revert "implemented new feature, calc_avg_salary"
    This reverts commit c7138d3505c22a0c24c0d9823099b027e6ccfa0f.

--- employee_info.py
+++ employee_info.py
@@ -21,11 +21,9 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
-    
+
     #add your implementation to calculate here
-    for item in employee_data:
-        total=total+int(item["salary"])
-    average=total/len(employee_data)
+
 
     return average
 
@@ -33,7 +31,7 @@ def get_employees_by_dept(department):
     result = []
 
     # Add your implementation from here
-    
+
 
     return result
 
